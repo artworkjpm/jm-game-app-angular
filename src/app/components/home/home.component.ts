8e98f2facf2482b7aeef64385f8db9d3d2f10ae6
John <johnnymoran44@hotmail.com>
2019-07-25 11:09:34 +0200

    finally have the correct and incorrect images working

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,44 +1,98 @@
 import { Component, OnInit } from "@angular/core";
 import { AppService } from "../../services/api.service";
 
+declare var $: any;
+
+const RandomMath = () => {
+  return Math.floor(Math.random() * 20);
+};
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-  Images: any;
+  CartoonImage: string;
+  PhotoWinner: string;
+  PhotoRandom1: string;
+  PhotoRandom2: string;
   FixedImage: any;
   searchInput: string;
+  winner: boolean = false;
+  wrong1: boolean = false;
+  wrong2: boolean = false;
 
   constructor(public appService: AppService) {}
 
-  ngOnInit() {
-    this.getFixedImage();
+  ngOnInit(): void {}
+  //using jquery to randomize the three photo options
+  randomize() {
+    var cards = $(".random");
+    for (var i = 0; i < cards.length; i++) {
+      var target = Math.floor(Math.random() * cards.length - 1) + 1;
+      var target2 = Math.floor(Math.random() * cards.length - 1) + 1;
+      cards.eq(target).before(cards.eq(target2));
+    }
   }
 
-  submitSearch(text: string) {
-    console.log(text);
-    this.searchInput = text;
-    this.getImages();
+  rightFire() {
+    this.winner = !this.winner;
   }
 
-  getImages() {
-    let searchItem = this.searchInput;
-    return this.appService
-      .getImages(searchItem)
-      .subscribe(
-        data => (
-          (this.Images = data.hits[Math.floor(Math.random() * 20)]),
-          console.log(data.hits, data.hits.length)
-        ),
-        error => console.log(error)
-      );
+  hidit() {
+    $(".alert-success").hide();
+    this.winner = false;
+  }
+
+  wrong1Fire() {
+    this.wrong1 = !this.wrong1;
+    setTimeout(() => {
+      $(".wrong1").hide(), (this.wrong1 = false);
+    }, 2000);
+  }
+
+  wrong2Fire() {
+    this.wrong2 = !this.wrong2;
+    setTimeout(() => {
+      $(".wrong2").hide(), (this.wrong2 = false);
+    }, 2000);
   }
 
   getFixedImage() {
     return this.appService.fixedImage().subscribe(data => {
       this.FixedImage = data.hits[0];
     });
+  }
+
+  submitSearch(text: string) {
+    console.log(text);
+    this.searchInput = text;
+    this.wrong1 = false;
+    this.CartoonChooser();
+    this.PhotoWinnerChooser();
+    this.randomize();
+  }
+
+  CartoonChooser() {
+    let searchItem = this.searchInput;
+    return this.appService
+      .CartoonChooser(searchItem)
+      .subscribe(data => ((this.CartoonImage = data.hits[Math.floor(Math.random() * 20)]), console.log(this.CartoonImage, data)), error => console.log(error));
+  }
+
+  RandomMath() {
+    return Math.floor(Math.random() * 20);
+  }
+
+  PhotoWinnerChooser() {
+    let searchItem = this.searchInput;
+    this.appService
+      .PhotoWinnerChooser(searchItem)
+      .subscribe(data => ((this.PhotoWinner = data.hits[RandomMath()]), console.log(this.PhotoWinner, data)), error => console.log(error));
+
+    this.appService.PhotoWinnerChooser("dog").subscribe(data => ((this.PhotoRandom1 = data.hits[RandomMath()]), console.log(this.PhotoRandom1, data)), error => console.log(error));
+
+    this.appService.PhotoWinnerChooser("cat").subscribe(data => ((this.PhotoRandom2 = data.hits[RandomMath()]), console.log(this.PhotoRandom2, data)), error => console.log(error));
   }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,26 +2,25 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 
+const API_KEY = "538780-df31edf39b820c4de87a19f6f";
+const urlPath = "https://pixabay.com/api/?key=";
+
 @Injectable()
 export class AppService {
-  constructor(private httpClient: HttpClient) {}
-
-  getImages(searchItem: string): Observable<any> {
-    const API_KEY = "538780-df31edf39b820c4de87a19f6f";
-    const URL =
-      "https://pixabay.com/api/?key=" +
-      API_KEY +
-      "&q=" +
-      encodeURIComponent(searchItem) +
-      "&image_type=illustration";
-    const api = URL;
-    return this.httpClient.get(api);
-  }
+  constructor(public httpClient: HttpClient) {}
 
   fixedImage(): Observable<any> {
-    const URL =
-      "https://pixabay.com/api/?key=538780-df31edf39b820c4de87a19f6f&id=158463";
-    const api = URL;
-    return this.httpClient.get(api);
+    const URL = "https://pixabay.com/api/?key=538780-df31edf39b820c4de87a19f6f&id=158463";
+    return this.httpClient.get(URL);
+  }
+
+  CartoonChooser(searchItem: string): Observable<any> {
+    const URL = urlPath + API_KEY + "&q=" + encodeURIComponent(searchItem) + "&image_type=illustration";
+    return this.httpClient.get(URL);
+  }
+
+  PhotoWinnerChooser(searchItem: string): Observable<any> {
+    const URL = urlPath + API_KEY + "&q=" + encodeURIComponent(searchItem) + "&image_type=photo";
+    return this.httpClient.get(URL);
   }
 }
